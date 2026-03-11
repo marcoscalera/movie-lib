4060d5d9518648b0354b745b3bac6f0d7cefba9b
unknown <caleramp@gmail.com>
2026-03-11 12:09:02 -0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom'
+import './App.css'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar />
+      <h1>Movie Lib TMDB</h1>
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+
+const NavBar = () => {
+  return (
+      <nav id="navbar">
+        <h2>
+        <Link to="/">
+          <BiCameraMovie /> MovieLib
+        </Link>
+        </h2>
+        <form>
+          <input type="text" placeholder="busque um filme" />
+            <button type="submit">
+              <BiSearchAlt2 />
+            </button>
+        </form>
+      </nav>
+  )
+}
+
+export default NavBar
