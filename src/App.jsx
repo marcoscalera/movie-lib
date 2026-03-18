aa3b67f1cc1000f3a849b863da081966d505b11a
unknown <caleramp@gmail.com>
2026-03-18 12:35:36 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import NavBar from './components/NavBar'
+import MovieCard from './components/MovieCard'
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
-      <h1>Movie Lib TMDB</h1>
       <Outlet />
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react"
+import MovieCard from "../components/MovieCard"
+
+//url
+const moviesURL = import.meta.env.VITE_API
+//key
+const apiKey = import.meta.env.VITE_API_KEY
+
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+    setTopMovies(data.results)
+  }
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+    getTopRatedMovies(topRatedUrl)
+  })
+  
   return (
-    <div>
-      Home
+    <div className="container">
+      <h2 className="title">Melhores FIlmes</h2>
+      <div className="movies-container">
+          {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
     </div>
   )
 }
